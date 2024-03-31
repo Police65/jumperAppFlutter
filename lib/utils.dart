7ed6e112f7e53a6a6ee7b2b38c4a93bb4ee280ea
Police65 <65681291+Police65@users.noreply.github.com>
2024-03-31 18:38:03 -0400

    ........

--- lib/utils.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/foundation.dart';
-
-bool get isIOS => defaultTargetPlatform == TargetPlatform.iOS;
-
-bool get isAndroid => defaultTargetPlatform == TargetPlatform.android;
-
-bool get isWeb => kIsWeb;
-
-bool get isMobile => isIOS || isAndroid;
