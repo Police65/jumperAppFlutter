41ecb4b7ba99d7281e1f12b13c2e21d3d6cccf35
Police65 <65681291+Police65@users.noreply.github.com>
2024-03-31 22:04:30 -0400

    Dios

--- lib/high_scores.dart
+++ lib/high_scores.dart
@@ -1,61 +1,17 @@
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:http/http.dart' as http;
-import 'package:supabase/supabase.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class HighScores {
-  static late final SharedPreferences prefs;
-  static final highScores = List.filled(5, 0, growable: false);
-
-  static const String SUPABASE_CLIENT_ANON_KEY =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yaHhzZXJud3hhb3Vkb2V6a2doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4NTgwNzMsImV4cCI6MjAyNzQzNDA3M30.tp6tC88PsPxInGhLF6dz2Rt4N_bLEDAnp1Jm3a1E254';
-  static const String SUPABASE_URL =
-      'https://nrhxsernwxaoudoezkgh.supabase.co/rest/v1/puntaje';
-
-  static Future<void> load() async {
-    prefs = await SharedPreferences.getInstance();
-    for (int i = 0; i < 5; i++) {
-      int score = prefs.getInt('score$i') ?? 0;
-      highScores[i] = score;
-    }
-  }
-
   static Future<void> saveNewScore(int score) async {
-    for (int i = 0; i < 5; i++) {
-      if (highScores[i] < score) {
-        for (int j = 4; j > i; j--) {
-          highScores[j] = highScores[j - 1];
-        }
-        highScores[i] = score;
-        break;
-      }
-    }
+    final supabaseClient = Supabase.instance.client;
 
-    for (int i = 0; i < 5; i++) {
-      await prefs.setInt('score$i', highScores[i]);
-    }
+    final response =
+        await supabaseClient.from('puntaje').insert({'score': score}).execute();
 
-    // Envía los puntajes a Supabase
-    await sendScoresToSupabase(highScores);
-  }
-
-  static Future<void> sendScoresToSupabase(List<int> scores) async {
-    final headers = {
-      'apikey': SUPABASE_CLIENT_ANON_KEY,
-      'Authorization': 'Bearer $SUPABASE_CLIENT_ANON_KEY',
-      'Content-Type': 'application/json',
-    };
-
-    final jsonData = scores.map((score) => {'score': score}).toList();
-    final response = await http.post(
-      SUPABASE_URL,
-      headers: headers,
-      body: jsonData,
-    );
-
-    if (response.statusCode == 200) {
-      print('Puntajes enviados exitosamente a Supabase');
+    if (response.error != null) {
+      print(
+          'Error al guardar el puntaje en Supabase: ${response.error!.message}');
     } else {
-      print('Error al enviar los puntajes a Supabase: ${response.statusCode}');
+      print('Puntaje guardado exitosamente en Supabase');
     }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,16 @@ import 'package:new_super_jumper/my_game.dart';
 import 'package:new_super_jumper/navigation/routes.dart';
 import 'package:new_super_jumper/ui/game_over_menu.dart';
 import 'package:new_super_jumper/ui/pause_menu.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Flame.device.fullScreen();
+
+  const supabaseUrl = 'https://nrhxsernwxaoudoezkgh.supabase.co';
+  const supabaseKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5yaHhzZXJud3hhb3Vkb2V6a2doIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE4NTgwNzMsImV4cCI6MjAyNzQzNDA3M30.tp6tC88PsPxInGhLF6dz2Rt4N_bLEDAnp1Jm3a1E254';
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
 
   await HighScores.load();
   await Assets.load();
